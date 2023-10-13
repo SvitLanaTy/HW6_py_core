8381d2ad385db1fdf13dc80a08c7c0478d0fcb55
SvitLanaTy <stymchenko@gmail.com>
2023-10-13 19:13:12 +0300

    first commit

---
+++ file_parser.py
@@ -0,0 +1,81 @@
+import sys
+from pathlib import Path
+
+JPEG_IMAGES = []
+JPG_IMAGES = []
+PNG_IMAGES = []
+SVG_IMAGES = []
+MP3_AUDIO = []
+OGG_AUDIO = []
+WAV_AUDIO = []
+AMR_AUDIO = []
+AVI_VIDEO = []
+MP4_VIDEO = []
+MOV_VIDEO = []
+MKV_VIDEO = []
+DOC_DOCUMENTS = []
+DOCX_DOCUMENTS = []
+TXT_DOCUMENTS = []
+PDF_DOCUMENTS = []
+XLSX_DOCUMENTS = []
+PPTX_DOCUMENTS = []
+MY_OTHER = []
+ARCHIVES = []
+
+REGISTER_EXTENSION = {
+    'JPEG': JPEG_IMAGES,
+    'JPG': JPG_IMAGES,
+    'PNG': PNG_IMAGES,
+    'SVG': SVG_IMAGES,
+    'MP3': MP3_AUDIO,
+    'OGG': OGG_AUDIO,
+    'WAV': WAV_AUDIO,
+    'AMR': AMR_AUDIO,
+    'AVI': AVI_VIDEO,
+    'MP4': MP4_VIDEO,
+    'MOV': MOV_VIDEO,
+    'MKV': MKV_VIDEO,
+    'DOC': DOC_DOCUMENTS,
+    'DOCX': DOCX_DOCUMENTS,
+    'TXT': TXT_DOCUMENTS,
+    'PDF': PDF_DOCUMENTS,
+    'XLSX': XLSX_DOCUMENTS,
+    'PPTX': PPTX_DOCUMENTS,
+    'ZIP': ARCHIVES,
+    'GZ': ARCHIVES,
+    'TAR': ARCHIVES,
+}
+
+FOLDERS = []
+EXTENSIONS = set()
+UNKNOWN = set()
+
+
+def get_extension(name: str) -> str:
+    return Path(name).suffix[1:].upper()
+
+def scan(folder: Path):
+    for item in folder.iterdir():
+        if item.is_dir():
+            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'MY_OTHER'):
+                FOLDERS.append(item)
+                scan(item)
+            continue
+        
+        extension = get_extension(item.name)
+        full_name = folder / item.name
+        if not extension:
+            MY_OTHER.append(full_name)
+        else:
+            try:
+                ext_reg = REGISTER_EXTENSION[extension]
+                ext_reg.append(full_name)
+                EXTENSIONS.add(extension)
+            except KeyError:
+                UNKNOWN.add(extension)
+                MY_OTHER.append(full_name)
+
+
+
+
+
---
+++ normalize.py
@@ -0,0 +1,20 @@
+import re
+
+CYRILLIC_SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ'
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "u", "ja", "je", "ji", "g")
+
+TRANS = dict()
+
+for cyrillic, latin in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(cyrillic)] = latin
+    TRANS[ord(cyrillic.upper())] = latin.upper()
+
+
+def normalize(name: str) -> str:
+    if name.rfind(".") != (-1 | 0):
+        base_name, ext = name.rsplit('.', 1)
+        translate_name = re.sub(r'\W', '_', base_name.translate(TRANS))
+        return f"{translate_name}.{ext}"
+    else:
+        return re.sub(r'\W', '_', name.translate(TRANS))
